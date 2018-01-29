a2f8cf5848cf3dfd8b6291686708280d048a3979
Matthew Topham <mat.topham@gmail.com>
2018-01-29 20:45:38 +0000

    Add project files.

---
+++ Lan_ConnectionManager/ConnectionManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lan_ConnectionManager
+{
+    // Connection authentication
+
+    /// <summary>
+    /// Finds other servers on the LAN and connects or starts a new server.
+    /// </summary>
+    public class ConnectionManager
+    {
+        /// <summary>
+        /// Finds all servers on the local network.
+        /// </summary>
+        public void GetAvailableConnection()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new server instance.
+        /// </summary>
+        public void Start()
+        {
+
+        }
+    }
+}
